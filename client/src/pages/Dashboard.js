f6523ba3c3473ed375fe5200cc45c91126b82639
3agleFF67c1 <ybahalker@gmail.com>
2021-04-23 18:54:37 +0300

    Header added

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../UserContext';
-
+import Header from '../components/Header';
   
 const Dashboard = () => {
   const { user, setUser } = useContext(UserContext);
   return (
-    <p>{JSON.stringify(user)}</p>
-  )
+    <div>
+      <Header user={user} />
+      <p>{JSON.stringify(user)}</p>
+    </div>
+  );
 }
 
 export default Dashboard;
